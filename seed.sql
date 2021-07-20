962697d8c70f349d9aeeb7a0248b7b7c65748bd8
Matilda Brantley <matildahbrantley@gmail.com>
2021-07-20 16:30:29 -0400

    Created seed tables for employees, roles and departments, with 3 departments, 2 roles per department and one employee per each role

--- seed.sql
+++ seed.sql
@@ -3,11 +3,62 @@ CREATE DATABASE employee_DB;
 
 USE employee_DB;
 
-CREATE TABLE Employees(
+CREATE TABLE Departments(
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO Departments (name)
+VALUES ("Sales");
+INSERT INTO Departments (name)
+VALUES ("Legal");
+INSERT INTO Departments (name)
+VALUES ("Software Development");
+
+
+CREATE TABLE Employees(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO Employees (first_name, last_name, role_id, manager_id)
+VALUES ("Bob", "Smith", 1, 1);
+INSERT INTO Employees (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 2, 1);
+INSERT INTO Employees (first_name, last_name, role_id, manager_id)
+VALUES ("Jane", "Smith", 3, 1);
+INSERT INTO Employees (first_name, last_name, role_id, manager_id)
+VALUES ("Jessica", "Smith", 4, 1);
+INSERT INTO Employees (first_name, last_name, role_id, manager_id)
+VALUES ("Finame", "Laname", 5, 1);
+INSERT INTO Employees (first_name, last_name, role_id, manager_id)
+VALUES ("Omy", "Goodness", 6, 1);
+
+
+CREATE TABLE Roles(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO Roles (title, salary, department_id)
+VALUES ("Salesperson", 80000, 1);
+INSERT INTO Roles (title, salary, department_id)
+VALUES ("Traveling Salesman", 81000, 1);
+INSERT INTO Roles (title, salary, department_id)
+VALUES ("Sales Manager", 90000, 1);
+INSERT INTO Roles (title, salary, department_id)
+VALUES ("Lawyer", 200000, 2);
+INSERT INTO Roles (title, salary, department_id)
+VALUES ("Head Lawyer", 250000, 2);
+INSERT INTO Roles (title, salary, department_id)
+VALUES ("Software Developer", 70000, 3);
+INSERT INTO Roles (title, salary, department_id)
+VALUES ("Lead Developer", 100000, 3);
